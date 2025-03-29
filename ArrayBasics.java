d842000b1d833922c1cdfdb70985ae893cd40ce3
Sairaj Panwalkar <sairajpanwalkar16@gmail.com>
2025-03-29 09:16:04 +0530

    Added ArrayBasics.java

---
+++ ArrayBasics.java
@@ -0,0 +1,8 @@
+public class ArrayBasics {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Element at index " + i + ": " + arr[i]);
+        }
+    }
+}
