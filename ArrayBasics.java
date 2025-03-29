7203e0a186d7f9ecfe494ad77539460c5f25869b
Sairaj Panwalkar <sairajpanwalkar16@gmail.com>
2025-03-29 09:30:55 +0530

    Updated output message in ArrayBasics.java

--- ArrayBasics.java
+++ ArrayBasics.java
@@ -2,7 +2,10 @@ public class ArrayBasics {
     public static void main(String[] args) {
         int[] arr = {10, 20, 30, 40};
         for (int i = 0; i < arr.length; i++) {
-            System.out.println("Element at index " + i + ": " + arr[i]);
+            System.out.println("Value at position " + i + " is: " + arr[i]);
+		
         }
+
+	System.out.println("Array length: " + arr.length);
     }
 }
